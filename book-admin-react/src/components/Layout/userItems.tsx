c2f6f2d5932c5f006675582536a98424500dedfd
RF0606 <15rf17@queensu.ca>
2024-07-03 18:43:45 -0400

    add login page

--- book-admin-react/src/components/Layout/userItems.tsx
+++ book-admin-react/src/components/Layout/userItems.tsx
@@ -1,12 +1,25 @@
-import { MenuProps } from "antd";
+import { logout } from "@/api/user";
+import { MenuProps, message } from "antd";
+import Link from "next/link";
+import router from "next/router";
 
 export const USER_ITEMS: MenuProps["items"] = [
     {
-        label: "用户中心",
+        label: <Link href="/user/edit/id">用户中心</Link>,
         key: "1",
     },
     {
-        label: "登出",
+        label: (
+            <span
+                onClick={async () => {
+                    await logout();
+                    message.success("登出成功");
+                    router.push("/login");
+                }}
+            >
+                登出
+            </span>
+        ),
         key: "2",
     },
 ];
